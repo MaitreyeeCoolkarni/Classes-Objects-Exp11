1a073557887ebda4602ddf3c7dd7c54d7160b39e
MaitreyeeCoolkarni <dunkul@gmail.com>
2025-08-14 11:56:40 +0530

    Create Calc.cpp

---
+++ Calc.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+#include<string>
+
+int main() {
+    
+    class Calc{
+       public:
+       int A;
+       int B;
+       
+       void add(){
+           cout<<(A+B);
+       }
+       
+       void subtract(){
+           cout<<(A-B);
+       }
+       
+       void divide(){
+        cout<<(A/B);
+       }
+       
+       void multiply(){
+        cout<<(A*B);
+       }
+       
+       
+    };
+    
+Calc C1;
+cout<<"Enter the number ";
+cin>>C1.A;
+cout<<"Enter the number 2 ";
+cin>>C1.B;
+
+    cout << "The addition is: "<<endl;
+    C1.add();
+    
+    cout<<" "<<endl;
+
+    cout << "The subtraction is: "<<endl;
+    C1.subtract();
+    
+    cout<<" "<<endl;
+
+    cout << "The division is: "<<endl;
+    C1.divide();
+    
+    cout<<" "<<endl;
+
+    cout << "The multiplication is: "<<endl;
+    C1.multiply();
+    
+    cout<<" "<<endl;
+
+    return 0;
+}
